086e819ab40f661807e58d488b2ecebfcb9f1f38
Jordan Heale <webmaster@pridit.co.uk>
2022-12-30 20:30:45 +0000

    refactor(revive): images -> textures, remove reference to unused blood_3

--- revive/dialog/titles.hpp
+++ revive/dialog/titles.hpp
@@ -18,7 +18,7 @@ class d_revive_blood_1 {
             y = "SafeZoneY";
             w = "SafeZoneWAbs + 0.05";
             h = "SafeZoneH + 0.05";
-            text = "revive\images\blood_1.paa";
+            text = "revive\textures\blood_1.paa";
         };
     };
 };
@@ -43,32 +43,7 @@ class d_revive_blood_2 {
             y = "SafeZoneY";
             w = "SafeZoneWAbs + 0.05";
             h = "SafeZoneH + 0.05";
-            text = "revive\images\blood_2.paa";
-        };
-    };
-};
-
-class d_revive_blood_3 {
-    idd = -1;
-    movingEnable = 0;
-    duration = 2;
-    fadein = 0.1;
-    fadeout = 8;
-    name = "d_revive_blood_3";
-    class controls {
-        class ScreenBlood_BG {
-            idc=-1;
-            type = 0;
-            style = 48;
-            colorBackground[] = {0, 0, 0, 0};
-            colorText[] = {0.9, 0.9, 0.9, 0.8};
-            font = "Bitstream";
-            sizeEx = 0.023;
-            x = "SafeZoneXAbs";
-            y = "SafeZoneY";
-            w = "SafeZoneWAbs + 0.05";
-            h = "SafeZoneH + 0.05";
-            text = "revive\images\blood_3.paa";
+            text = "revive\textures\blood_2.paa";
         };
     };
 };
