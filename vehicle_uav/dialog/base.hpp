f15552300bf87a9cdaf683d2b4f669378b20b7f2
Jordan Heale <webmaster@pridit.co.uk>
2025-03-12 20:39:25 +0000

    chore: tabs -> spaces

--- vehicle_uav/dialog/base.hpp
+++ vehicle_uav/dialog/base.hpp
@@ -14,19 +14,19 @@ class XD_VehicleUavDialog {
         };
         class RscVehicleUavMapControl: RscMapWithListMapControl
         {
-			idc = 10000;
+            idc = 10000;
             onMouseButtonDown = "if !([lbCurSel 200, -1] call BIS_fnc_areEqual) then {call d_fnc_vehicle_uav_populate}";
             onMouseZChanged = "if !([lbCurSel 200, -1] call BIS_fnc_areEqual) then {call d_fnc_vehicle_uav_populate}";
         };
         class RscVehicleUavListbox: RscMapWithListListbox
         {
-			idc = 200;
+            idc = 200;
             onLBSelChanged = "['vehicle_uav', _this select 1] call d_fnc_interface_switch";
             onLBDblClick = "call d_fnc_vehicle_uav_action";
         };
         class RscVehicleUavButtonAction: RscMapWithListButtonAction
         {
-			idc = 300;
+            idc = 300;
             action = "call d_fnc_vehicle_uav_action";
         };
         class RscVehicleUavButtonClose: RscMapWithListButtonClose {};
